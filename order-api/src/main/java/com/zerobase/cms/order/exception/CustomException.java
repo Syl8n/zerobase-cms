821bde8e934d4c265815c4f2daf5c50b331aa767
Syl8n <magi8520@gmail.com>
2022-12-31 00:02:51 +0900

    feat: [009-init-order-api] order-api 초기 세팅

---
+++ order-api/src/main/java/com/zerobase/cms/order/exception/CustomException.java
@@ -0,0 +1,15 @@
+package com.zerobase.cms.order.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException{
+    private final ErrorCode errorCode;
+    private final int status;
+
+    public CustomException(ErrorCode errorCode){
+        super(errorCode.getDetail());
+        this.errorCode = errorCode;
+        this.status = errorCode.getHttpStatus().value();
+    }
+}
