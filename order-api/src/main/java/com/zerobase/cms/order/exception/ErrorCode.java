c532100c756576489dfeeb2599d3946485edc8cc
Syl8n <magi8520@gmail.com>
2022-12-31 15:53:57 +0900

    feat: [011-product-modify] 상품 & 아이템 수정
    상품과 아이템에 대한 수정

--- order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
+++ order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     PRODUCT_NOT_FOUND(HttpStatus.BAD_REQUEST, "상품을 찾을 수 없습니다."),
+    ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "아이템을 찾을 수 없습니다."),
     SAME_ITEM_NAME(HttpStatus.BAD_REQUEST, "아이템 명 중복입니다.")
     ;
     private final HttpStatus httpStatus;
