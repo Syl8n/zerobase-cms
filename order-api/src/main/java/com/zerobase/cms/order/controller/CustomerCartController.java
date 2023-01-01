47089ca5afd88598fd6cbc1c59014fe3a96387ea
Syl8n <magi8520@gmail.com>
2023-01-01 17:07:15 +0900

    feat: [016-cart-update] 장바구니 갱신
    조회 로직 재사용하여 생산성 향상

--- order-api/src/main/java/com/zerobase/cms/order/controller/CustomerCartController.java
+++ order-api/src/main/java/com/zerobase/cms/order/controller/CustomerCartController.java
@@ -2,11 +2,13 @@ package com.zerobase.cms.order.controller;
 
 import com.zerobase.cms.order.application.CartApplication;
 import com.zerobase.cms.order.domain.product.AddProductCartForm;
+import com.zerobase.cms.order.domain.redis.Cart;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,13 +25,21 @@ public class CustomerCartController {
     @PostMapping
     public ResponseEntity<?> addCart(
         @RequestHeader(name = "X-Auth-Token") String token,
-        @RequestBody AddProductCartForm form){
+        @RequestBody AddProductCartForm form) {
         return ResponseEntity.ok(cartApplication.addCart(provider.getUserVo(token).getId(), form));
     }
 
     @GetMapping
-    public ResponseEntity<?> viewCart(@RequestHeader(name = "X-Auth-Token") String token){
+    public ResponseEntity<?> viewCart(@RequestHeader(name = "X-Auth-Token") String token) {
         return ResponseEntity.ok(cartApplication.getCart(provider.getUserVo(token).getId()));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateCart(
+        @RequestHeader(name = "X-Auth-Token") String token,
+        @RequestBody Cart cart) {
+        return ResponseEntity.ok(
+            cartApplication.updateCart(provider.getUserVo(token).getId(), cart));
     }
 
 }
